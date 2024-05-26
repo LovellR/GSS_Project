31e23148565827b687b60f40e0417bed8789deaa
Mina Dong <minadong404@gmail.com>
2024-05-26 16:07:00 +0900

    app.js 수정 후 menu.js연결 및 이미지 로고 삽입

--- oss-project/src/App.js
+++ oss-project/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './JS/Home'; // Ensure these paths are correct
 import Detail from './JS/Detail'; // Ensure these paths are correct
 import Search from './JS/Search'; // Ensure these paths are correct
+import Menu from './JS/Menu'; // Ensure these paths are correct
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,20 +13,7 @@ class App extends Component {
     return (
       <div className="App">
         <Router>
-          <header className="App-header">
-            <nav>
-              <Link to="/">Home</Link> | 
-              <Link to="/detail">Detail</Link> | 
-              <Link to="/search">Search</Link>
-            </nav>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            </a>
-          </header>
+            <Menu />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/detail" element={<Detail />} />
--- oss-project/src/JS/Menu.js
+++ oss-project/src/JS/Menu.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './menu.css'
 
 const Menu = () => {
     return (
-        <div>
+        <div className='nav-container'>     
+            <img className="logo-nav-img" src ="https://www.cbnu.ac.kr/site/www/images/contents/cts596_img01.png" alt="plant_wep" />
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/Detail">Detail</Link></li>
