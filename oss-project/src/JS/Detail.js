d2c5b39a038722e80d56eb13d0b5249843985718
choiyoo <choi.yoo.lim.12@gmail.com>
2024-05-29 03:04:13 +0900

    검색창 생성

--- oss-project/src/JS/Detail.js
+++ oss-project/src/JS/Detail.js
@@ -3,9 +3,11 @@ import React from 'react';
 const Detail = () => {
     return (
         <div>
-            <h2>
-                상세정보 페이지
-            </h2>
+            <h2>개인정보 기반 약물 찾기</h2>
+            <div className="search-container">
+                <input type="text" placeholder="약물 이름을 입력하세요..." />
+                <button type="submit">검색</button>
+            </div>
         </div>
     );
 };
