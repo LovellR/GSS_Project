17130ee86fb297127f67e570fc729241e81d00dd
LovellR <129665363+LovellR@users.noreply.github.com>
2024-05-28 15:09:17 +0900

    테이블 지정 및 로드 속성 설정

--- DB/main.py
+++ DB/main.py
@@ -14,3 +14,9 @@ engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{databa
 # CSV 파일 읽기
 csv_file_path = '한국의약품안전관리원_노인주의약물_20230701.csv'
 df = pd.read_csv(csv_file_path)
+
+# 데이터프레임을 MySQL 테이블로 저장
+table_name = '노인주의약물'  # 테이블 이름을 지정합니다
+df.to_sql(table_name, con=engine, if_exists='append', index=False)
+
+print(f"CSV 파일이 MySQL DB의 {table_name} 테이블에 성공적으로 로드되었습니다.")
