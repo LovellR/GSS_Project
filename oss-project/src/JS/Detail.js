d4c9c8b9d5575f6ef3ce81aab9c24ff8e169aa27
choiyoo <choi.yoo.lim.12@gmail.com>
2024-05-29 03:31:20 +0900

    개인정보 기반 약물 검색하는 컴포넌트 생성

--- oss-project/src/JS/Detail.js
+++ oss-project/src/JS/Detail.js
@@ -1,13 +1,11 @@
 import React from 'react';
+import PersonalEditor from './PersonalEditor';
 
 const Detail = () => {
     return (
         <div>
             <h2>개인정보 기반 약물 찾기</h2>
-            <div className="search-container">
-                <input type="text" placeholder="약물 이름을 입력하세요..." />
-                <button type="submit">검색</button>
-            </div>
+            <PersonalEditor />
         </div>
     );
 };
---
+++ oss-project/src/JS/PersonalEditor.js
@@ -0,0 +1,10 @@
+const PersonalEditor = () => {
+    return (
+        <div className="editor_wrapper">
+            <input type="text" placeholder="약물 이름을 입력하세요..." />
+            <button type="submit">검색</button>
+        </div>
+    );
+};
+
+export default PersonalEditor;
