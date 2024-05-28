46996628b1d08c7698db80f267628465527ab1d0
LovellR <129665363+LovellR@users.noreply.github.com>
2024-05-28 16:55:56 +0900

    CSV 인코딩 타입 에러 CP949인코더

---
+++ DB/CSVLoader_CP949_Encording.py
@@ -0,0 +1,30 @@
+import chardet
+import pandas as pd
+from sqlalchemy import create_engine
+
+# MySQL 연결 설정
+user = 'root'
+password = 'root'
+host = '127.0.0.1'
+port = 3306
+database = 'druginfos'
+
+# SQLAlchemy 엔진 생성
+engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}')
+
+# CSV 파일 경로지정
+csv_file_path = '한국의약품안전관리원_병용금기약물_20230705.csv'
+
+# 파일 UTF-8 인코딩 감지
+with open(csv_file_path, 'rb') as f:
+    result = chardet.detect(f.read(10000))
+    file_encoding = result['encoding']
+
+# 감지된 인코딩을 사용하여 CSV 파일 읽기
+df = pd.read_csv(csv_file_path, encoding='cp949')
+
+# 데이터프레임을 MySQL 테이블로 저장
+table_name = '병용금기약물'  # 테이블 이름을 지정합니다
+df.to_sql(table_name, con=engine, if_exists='append', index=False)
+
+print(f"CSV 파일이 MySQL DB의 {table_name} 테이블에 성공적으로 로드되었습니다.")
