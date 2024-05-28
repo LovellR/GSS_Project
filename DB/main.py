1bc4a7de7591f661f2f2da284b8d768fe452a225
LovellR <129665363+LovellR@users.noreply.github.com>
2024-05-28 15:06:34 +0900

    CSV 파일 위치 지정 및 Loader 엔진 생성

--- DB/main.py
+++ DB/main.py
@@ -7,3 +7,10 @@ password = 'root'
 host = 'localhost'
 port = 3306
 database = 'OSBP'
+
+# SQLAlchemy 엔진 생성
+engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}')
+
+# CSV 파일 읽기
+csv_file_path = '한국의약품안전관리원_노인주의약물_20230701.csv'
+df = pd.read_csv(csv_file_path)
