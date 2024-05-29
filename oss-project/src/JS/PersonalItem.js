0b243f43705e6cf8b0a4ec0c830120eb4e51c6cd
choiyoo <choi.yoo.lim.12@gmail.com>
2024-05-29 12:40:01 +0900

    개인정보 기반 약물 아이템 스타일링

--- oss-project/src/JS/PersonalItem.js
+++ oss-project/src/JS/PersonalItem.js
@@ -1,9 +1,11 @@
+import "./css/PersonalItem.css";
+
 const PersonalItem = () => {
     return (
-        <div className="personal_item">
+        <div className="personal-item">
             <div className="title">약품명: </div>
             <div className="possible">복용가능여부: </div>
-            <div className="delete_btn">
+            <div className="delete-btn">
                 <button>delete</button>
             </div>
         </div>
