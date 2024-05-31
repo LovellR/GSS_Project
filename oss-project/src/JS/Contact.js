2cad5064a541b5b7c681bb07a7aedb5882178ea1
choiyoo <choi.yoo.lim.12@gmail.com>
2024-05-31 21:30:57 +0900

    문의 페이지 구성

--- oss-project/src/JS/Contact.js
+++ oss-project/src/JS/Contact.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            <h2>
-                문의페이지
-            </h2>
+        <div id="contact">
+            <div>
+                <h2>문의페이지</h2>
+            </div>
+            <form>
+                <div>
+                    <label >제목: </label>
+                    <input class="title" type="text" />
+                    <label >내용: </label>
+                    <textarea class="content" />
+                </div>
+                <div>
+                    <input type="submit" />
+                </div>
+            </form>
         </div>
+        
     );
 };
 
