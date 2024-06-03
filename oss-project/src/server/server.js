f9b8aad4f5e57f3f82c4492b3f6de3b4aafc34b2
Mina Dong <minadong404@gmail.com>
2024-06-03 16:44:00 +0900

    body-parser for POST

--- oss-project/src/server/server.js
+++ oss-project/src/server/server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const PORT =  process.env.PORT || 4000;
 const db = require('./config/db.js');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     console.log('root');
