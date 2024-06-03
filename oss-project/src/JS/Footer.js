5cb62f45da3c3eba98b43d2777a8b1a4d8e4d4b8
choiyoo <choi.yoo.lim.12@gmail.com>
2024-06-04 01:25:25 +0900

    오타 수정 및 개인정보 입력 스타일링

--- oss-project/src/JS/Footer.js
+++ oss-project/src/JS/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <hr/>
             <ul style={{color:'gray'}}>
                 <li>팀 이름: 경소소</li>
-                <li>강의명: 오픈소스개발프로젝트</li>
+                <li>강의명: 오픈소스기초프로젝트</li>
                 <li>팀원: 이준호, 동민아, 최유림</li>
             </ul>
         </div>
