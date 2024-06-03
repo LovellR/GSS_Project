8ceff2e4fe24bbc69e30c3d879c18d7f8f5422e4
choiyoo <choi.yoo.lim.12@gmail.com>
2024-06-04 01:28:13 +0900

    User.js를 Detail.js에 임포트

--- oss-project/src/JS/Detail.js
+++ oss-project/src/JS/Detail.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PersonalEditor from './PersonalEditor';
 import PersonalList from './PersonalList';
+import User from './User';
+
 const Detail = () => {
     return (
         <div>
             <h2>개인정보 기반 약물 찾기</h2>
+            <User />
             <PersonalEditor />
             <PersonalList />
         </div>
