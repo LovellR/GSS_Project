dc8a65a5f1f70457659fc639cfc3e4d386a70fa4
Mina Dong <minadong404@gmail.com>
2024-06-03 12:21:10 +0900

    병용금지 약물 데이터 서버 전송

--- oss-project/src/JS/Search.js
+++ oss-project/src/JS/Search.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
-//                병용금지페이지
+//병용금지페이지
 const Search = () => {
+    const [firstMedicine, setFisrsMedicine] = useState('');
+
+    const handleSubmit = async() =>{
+        const data = {
+            firstMedicine
+        }
+    };
+
+    try{
+        const response = await axios.post('server', data,
+        {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        console.log('server res: ', response.data);
+    }catch (errror){
+        console.error('error with the axios oper:', error);
+    }
+};
+
     return (
         <div>
             <h2>
@@ -11,8 +34,8 @@ const Search = () => {
             <div className="medicine">
                 <div className="medicine-info">
                     <span>첫번째 약 :</span>
-                    <input type="text" placeholder="검색창" />
-                    <button>저장</button>
+                    <input type="text" placeholder="검색창" value={firstMedicine} onChange={(e)=> setFisrsMedicine(e.target.value)}/>
+                    <button onClick={handleSubmit}>저장</button>
                 </div>
                 <div className="medicine-info">
                     <span>두번째 약 :</span>
