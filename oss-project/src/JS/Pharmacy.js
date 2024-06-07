346f03976e78395094a72e1b4b9dbbb107a36572
Mina Dong <minadong404@gmail.com>
2024-06-07 21:28:35 +0900

    지역선택 함수 생성

--- oss-project/src/JS/Pharmacy.js
+++ oss-project/src/JS/Pharmacy.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
 const Pharmacy = () => {
+    const [selectedRegion, setSelectedRegion] = useState('');
+    const [pharmacies, setPharmacies] = useState([]);
+    
+    useEffect(() => {
+        if (selectedRegion !== '') {
+            fetchPharmacies(selectedRegion);
+        }
+    }, [selectedRegion]);
+    
     return (
         <div>     
             <h2 className='no-border'>
                 약국 찾기 페이지
             </h2>
 
-            <select>
+            <select onChange={handleRegion} value={selectedRegion}>
                 <option value="">지역 선택</option>
                 <option value="서울시">서울시</option>
                 <option value="청주시">청주시</option>
