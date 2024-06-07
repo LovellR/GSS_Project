0d3e2ffa55a9359a24f3da512cce8ee7adf59284
choiyoo <choi.yoo.lim.12@gmail.com>
2024-06-07 15:48:05 +0900

    스타일링

--- oss-project/src/JS/Detail.js
+++ oss-project/src/JS/Detail.js
@@ -3,20 +3,21 @@ import PersonalEditor from './PersonalEditor';
 import PersonalList from './PersonalList';
 import User from './User';
 
+import "./css/Detail.css";
+
 const Detail = () => {
     const [visible, setVisible] = useState(false);
     
     return (
         <div>
             <h2>개인정보 기반 약물 찾기</h2>
-            <div>
+            <div className='input_personal'>
                 <button onClick={() => {
                     setVisible(!visible);
                 }}
                 >
                     {visible ? "개인 이력 입력 창 닫기" : "개인 이력 입력 창 열기"}
                 </button>
-                <hr />
                 {visible && <User />}
             </div> 
             <PersonalEditor />
