d73fbb58ecd2067038d7715e8db8c4ca4e756718
choiyoo <choi.yoo.lim.12@gmail.com>
2024-06-07 14:52:59 +0900

    개인 이력 입력 창 열고 닫기 기능

--- oss-project/src/JS/Detail.js
+++ oss-project/src/JS/Detail.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import { React, useState } from 'react';
 import PersonalEditor from './PersonalEditor';
 import PersonalList from './PersonalList';
 import User from './User';
 
 const Detail = () => {
+    const [visible, setVisible] = useState(false);
+    
     return (
         <div>
             <h2>개인정보 기반 약물 찾기</h2>
-            <User />
+            <div>
+                <button onClick={() => {
+                    setVisible(!visible);
+                }}
+                >
+                    {visible ? "개인 이력 입력 창 닫기" : "개인 이력 입력 창 열기"}
+                </button>
+                <hr />
+                {visible && <User />}
+            </div> 
             <PersonalEditor />
             <PersonalList />
         </div>
