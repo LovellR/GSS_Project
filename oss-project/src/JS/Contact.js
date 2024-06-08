382b821dda794eaf4e7bc4c5360d97406ffa13af
choiyoo <choi.yoo.lim.12@gmail.com>
2024-06-08 18:43:46 +0900

    문의 페이지 html 코드 수정

--- oss-project/src/JS/Contact.js
+++ oss-project/src/JS/Contact.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useRef} from 'react';
+import emailjs from "@emailjs/browser";
 import "./css/Contact.css";
 
 const Contact = () => {
+    const SERVICE_ID = "service_r0a0dxm";
+    const TEMPLATE_ID = "template_symu0op";
+    const PUBLIC_KEY = "oyhTWECKoFoWOfvAG";
+    const form = useRef();
+
     return (
         <div id="contact">
             <div>
                 <h2>문의페이지</h2>
             </div>
-            <form class="form">
+            <form ref={form} onSubmit={sendEmail} class="form">
                 <div>
-                    <input placeholder="제목" class="title" type="text" />
+                    <input 
+                        placeholder="제목" 
+                        name="title" 
+                        className="title" 
+                        type="text" 
+                    />
                     <br />
-                    <input placeholder="답변 받을 이메일을 적으시오" class="email" type="email" />
+                    <input 
+                        placeholder="답변 받을 이메일을 적으시오" 
+                        name='email'
+                        className="email" 
+                        type="email" 
+                    />
                     <br />
-                    <textarea placeholder="내용" class="content" />
+                    <textarea
+                        placeholder="내용"
+                        name='content'
+                        className="content" 
+                    />
                 </div>
                 <div>
-                    <input class="submit" type="submit" />
+                    <input class="submit" type="submit" value="전송" />
                 </div>
             </form>
         </div>
