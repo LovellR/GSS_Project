1744fe46b87345b3b911ae5171226c69025af0a1
choiyoo <choi.yoo.lim.12@gmail.com>
2024-06-08 13:15:40 +0900

    빈 입력 방지 기능

--- oss-project/src/JS/PersonalEditor.js
+++ oss-project/src/JS/PersonalEditor.js
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./css/PersonalEditor.css";
 
 const PersonalEditor = ({ onCreate }) => {
     const [content, setContent] = useState("");
+    const inputRef = useRef();
+
     const onChangeContent = (e) => {
         setContent(e.target.value);
     };
     const onSubmit = () =>{
+        if(!content){
+            inputRef.current.focus();
+            return;
+        }
         onCreate(content);
     };
 
@@ -15,6 +21,7 @@ const PersonalEditor = ({ onCreate }) => {
             <input 
                 type="text" 
                 placeholder="약물 이름을 입력하세요..." 
+                ref={inputRef}
                 value={content}
                 onChange={onChangeContent}
             />
