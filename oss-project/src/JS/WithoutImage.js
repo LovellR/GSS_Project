87cebbd4925172b9e56e28f474f39f27dfb0b20d
LovellR <129665363+LovellR@users.noreply.github.com>
2024-06-08 20:55:35 +0900

    사용할 변수 선언

--- oss-project/src/JS/WithoutImage.js
+++ oss-project/src/JS/WithoutImage.js
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 
 const WithoutImage = () => {
 
+const [identification, setIdentification] = useState('');
+const [productName, setProductName] = useState('');
+const [companyName, setCompanyName] = useState('');
+const [selectedType, setSelectedType] = useState('');
+const [selectedDivider, setSelectedDivider] = useState('');
+const [selectedShape, setSelectedShape] = useState('');
+const [selectedColor, setSelectedColor] = useState(''); 
+
 return (
+    
 }
 
 export default WithoutImage;
