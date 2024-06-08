799f2272e03581cbcf266df2871c44908d830a41
LovellR <129665363+LovellR@users.noreply.github.com>
2024-06-08 20:55:46 +0900

    배열 선언

--- oss-project/src/JS/WithoutImage.js
+++ oss-project/src/JS/WithoutImage.js
@@ -10,6 +10,11 @@ const [selectedDivider, setSelectedDivider] = useState('');
 const [selectedShape, setSelectedShape] = useState('');
 const [selectedColor, setSelectedColor] = useState(''); 
 
+const shapes = ['원형', '타원형', '장방형', '반원형', '삼각형', '사각형', '마름모형', '오각형', '육각형', '팔각형', '전체', '기타'];
+const dividers = ['없음', '(+)형', '(-)형', '기타', '전체'];
+const types = ['정제', '경질캡슐', '연질캡슐', '기타', '전체'];
+const colors = ['하양', '노랑', '주황', '분홍', '빨강', '갈색', '연두', '초록', '청록', '파랑', '남색', '자주', '보라', '회색', '검정', '투명', '전체'];
+
 return (
     
 }
