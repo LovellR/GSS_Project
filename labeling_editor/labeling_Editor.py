792d2fc457acd7cf46973bd468c86bd8a024fddd
LovellR <129665363+LovellR@users.noreply.github.com>
2024-06-08 13:27:11 +0900

    대우세파클러, 어린이용타스펜, 드로세프, 아주세프라딘 추가

--- labeling_editor/labeling_Editor.py
+++ labeling_editor/labeling_Editor.py
@@ -3,4 +3,8 @@ import json
 
 //약물 이름 및 위치 선언
 directories_and_drug_names = [
+    ('K-004562_json', '대우세파클러캡슐 250mg'),
+    ('K-004563_json', '어린이용타스펜정 160mg'),
+    ('K-004565_json', '드로세프캡슐 500mg'),
+    ('K-004569_json', '아주세프라딘캡슐 500mg'),
 ]
