ed426716bf8f467aae812aff31f376336d2c4340
choiyoo <choi.yoo.lim.12@gmail.com>
2024-06-08 18:08:08 +0900

    문의하는 사용자의 이메일 입력창 추가

--- oss-project/src/JS/Contact.js
+++ oss-project/src/JS/Contact.js
@@ -11,6 +11,8 @@ const Contact = () => {
                 <div>
                     <input placeholder="제목" class="title" type="text" />
                     <br />
+                    <input placeholder="답변 받을 이메일을 적으시오" class="email" type="email" />
+                    <br />
                     <textarea placeholder="내용" class="content" />
                 </div>
                 <div>
