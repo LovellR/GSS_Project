c2a6169914479f7351e3aadf9196a6fe76423a5b
LovellR <129665363+LovellR@users.noreply.github.com>
2024-06-08 20:48:55 +0900

    이미지 업로드 함수 생성

--- oss-project/src/JS/Detect.js
+++ oss-project/src/JS/Detect.js
@@ -2,5 +2,17 @@ import React, { useState } from 'react';
 import WithoutImage from './WithoutImage';
 
 const Detect = () => {
-    
+
+    const onUpload = (e) => {
+        const file = e.target.files[0];
+        
+        // 파일이 없거나 이미지 파일이 아닌 경우에 대한 유효성 검사
+        if (!file || !file.type.startsWith('image/')) {
+            alert('이미지 파일을 업로드해주세요.');
+            return;
+        }
+        };
+    };
+
+
 export default Detect;
